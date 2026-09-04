60e2fea74883504ac3a35fb5a3b6b2a0dd8609a4
Bastien <bastien-stachs@hotmail.fr>
2026-09-04 14:44:04 +0200

    Ajout de la connection au backend pour le déploiement

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Comment gérer différentes URLs pour le développement, la pré-production et la production ?
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
 });
 
 export default api;
